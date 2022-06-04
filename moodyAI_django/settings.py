986206bb3e87bb61870f8053b032be9f118053aa
Sara Costa-Maia <sara_cm88@hotmail.com>
2022-06-03 21:01:58 -0700

    settings file updated

--- moodyAI_django/settings.py
+++ moodyAI_django/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from decouple import config
 from moodyAIpy.apps import MoodyaipyConfig
+from django.conf import settings
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-up()%ccb$k2h(k@8in@hyd1yw&#vw(n7_#+-$d9_=*jg5r4w%#'
+SECRET_KEY = settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
